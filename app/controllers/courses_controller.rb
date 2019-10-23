bb8a1135833d306f4e6eb39cc78acd5a9f30ba6d
Michael Evans <mmevansjr@gmail.com>
2019-10-23 09:44:36 -0500

    form to create course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,20 @@ class CoursesController < ApplicationController
         courses = Course.all
         render json: courses
     end
+
+    def create
+        course = Course.create(courses_params)
+        if course.save
+            render json: {status: 'Course created successfully' }, status: :created
+        else
+            render json: { errors: course.errors.full_messages }, status: :bad_request
+        end
+    end
+
+    private
+
+    def courses_params
+        params.permit(:name, :subject, :start_time, :end_time, :start_date, :end_date, :user_id, :description)
+    end
+
 end
